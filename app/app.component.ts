d77ab9ae2c93f99eb3af3d78b7b74fa7d6853f87
Ken Pierce <kpierce8@gmail.com>
2016-10-12 11:12:07 -0700

    moved app.component template to html file

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,20 +4,7 @@ import { Component } from '@angular/core';
 @Component ({
     selector: 'my-app',
     styleUrls: [ 'app/app.component.css'],
-    template: `
-    <h1>{{ title }}</h1>
-    <nav>
-    <a routerLink="/dashboard"  routerLinkActive="active">Dashboard</a>
-    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <h3>If I put a bunch of stuff here</h3>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <router-outlet></router-outlet>
-    <p> the router outlet is where the stuff from the router links goes</p>
-    `
+    templateUrl: './app/app.component.html',
 })
 
 export class AppComponent {
